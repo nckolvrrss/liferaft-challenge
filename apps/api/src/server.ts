fbcbc1407e23f495fad550be80159f2398b9c6ff
nckolvrrss <nick.ross@endearhq.com>
2022-08-15 16:03:14 -0300

    rebuild

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,7 +1,9 @@
 import { json, urlencoded } from "body-parser";
 import express from "express";
+import fs from "fs";
 import morgan from "morgan";
 import cors from "cors";
+import { body, validationResult } from "express-validator";
 
 export const createServer = () => {
   const app = express();
@@ -11,9 +13,41 @@ export const createServer = () => {
     .use(urlencoded({ extended: true }))
     .use(json())
     .use(cors())
-    .get("/message/:name", (req, res) => {
-      return res.json({ message: `hello ${req.params.name}` });
-    })
+    .post(
+      "/message",
+      body("name").isString().withMessage("The name must be a string"),
+      body("email")
+        .isEmail()
+        .withMessage(
+          "The email address is not formatted correctly. Please try again."
+        ),
+      body("phoneNumber")
+        .isString()
+        .withMessage("The phone number must be a string"),
+      body("address.houseNumber")
+        .isInt()
+        .withMessage("The house number must be an integer"),
+      body("address.streetName")
+        .isString()
+        .withMessage("The street name must be an string"),
+      body("address.city").isString().withMessage("The city must be an string"),
+      body("address.stateProvince")
+        .isString()
+        .withMessage("The state/province must be an string"),
+      body("address.country")
+        .isString()
+        .withMessage("The country must be an string"),
+
+      (req: express.Request, res: express.Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.json({ errors: errors });
+        }
+        return res.json({
+          message: `Success! Your customer contact information has been added. ${req.body.address.city}`,
+        });
+      }
+    )
     .get("/healthz", (req, res) => {
       return res.json({ ok: true });
     });
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,51 +1,169 @@
 import { useEffect, useState } from "react";
-import { Button } from "ui";
 
 const API_HOST = process.env.NEXT_PUBLIC_API_HOST || "http://localhost:3001";
 
 export default function Web() {
-  const [name, setName] = useState<string>("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [houseNumber, setHouseNumber] = useState("");
+  const [streetName, setStreetName] = useState("");
+  const [city, setCity] = useState("");
+  const [stateProvince, setStateProvince] = useState("");
+  const [country, setCountry] = useState("");
   const [response, setResponse] = useState<{ message: string } | null>(null);
   const [error, setError] = useState<string | undefined>();
 
-  useEffect(() => {
-    setResponse(null);
-    setError(undefined);
-  }, [name]);
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setName(e.target.value);
+  interface FormDataType {
+    name: string;
+    email: string;
+    phoneNumber: string;
+    address: {
+      houseNumber: string;
+      streetName: string;
+      city: string;
+      stateProvince: string;
+      country: string;
+    };
+  }
+  const responseBody: FormDataType = {
+    name: "",
+    email: "",
+    phoneNumber: "",
+    address: {
+      houseNumber: "",
+      streetName: "",
+      city: "",
+      stateProvince: "",
+      country: "",
+    },
+  };
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    responseBody.name = name;
+    responseBody.email = email;
+    responseBody.phoneNumber = phoneNumber;
+    responseBody.address.houseNumber = houseNumber;
+    responseBody.address.streetName = streetName;
+    responseBody.address.city = city;
+    responseBody.address.stateProvince = stateProvince;
+    responseBody.address.country = country;
+    console.log(JSON.stringify(responseBody));
 
     try {
-      const result = await fetch(`${API_HOST}/message/${name}`);
+      const result = await fetch(`${API_HOST}/message`, {
+        method: "POST",
+        body: JSON.stringify(responseBody),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
       const response = await result.json();
-      setResponse(response);
+      if (response.message) {
+        setResponse(response);
+      }
+      if (response.errors) {
+        setError(JSON.stringify(response.errors.errors));
+      }
     } catch (err) {
       console.error(err);
       setError("Unable to fetch response");
     }
   };
 
-  const onReset = () => {
-    setName("");
+  const inputChangeHandler = (
+    setFunction: React.Dispatch<React.SetStateAction<string>>,
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setFunction(event.target.value);
   };
 
   return (
     <div>
-      <h1>Web</h1>
+      <h1>LifeRaft Challenge</h1>
       <form onSubmit={onSubmit}>
-        <label htmlFor="name">Name </label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          value={name}
-          onChange={onChange}
-        ></input>
-        <Button type="submit">Submit</Button>
+        <div>
+          <label htmlFor="name">Name</label>
+        </div>
+        <div>
+          <input
+            id="name"
+            onChange={(e) => inputChangeHandler(setName, e)}
+            type="text"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+        </div>
+        <div>
+          <input
+            id="email"
+            onChange={(e) => inputChangeHandler(setEmail, e)}
+            type="text"
+          />
+        </div>
+        <div>
+          <label htmlFor="phone_number">Phone Number</label>
+        </div>
+        <div>
+          <input
+            id="phone_number"
+            onChange={(e) => inputChangeHandler(setPhoneNumber, e)}
+            type="text"
+          />
+        </div>
+        <div>
+          <label htmlFor="house_number">House Number</label>
+        </div>
+        <div>
+          <input
+            id="house_number"
+            onChange={(e) => inputChangeHandler(setHouseNumber, e)}
+            type="int"
+          />
+        </div>
+        <div>
+          <label htmlFor="street_name">Street Name</label>
+        </div>
+        <div>
+          <input
+            id="street_name"
+            onChange={(e) => inputChangeHandler(setStreetName, e)}
+            type="string"
+          />
+        </div>
+        <div>
+          <label htmlFor="city">City</label>
+        </div>
+        <div>
+          <input
+            id="city"
+            onChange={(e) => inputChangeHandler(setCity, e)}
+            type="string"
+          />
+        </div>
+        <div>
+          <label htmlFor="state_province">State / Province</label>
+        </div>
+        <div>
+          <input
+            id="state_province"
+            onChange={(e) => inputChangeHandler(setStateProvince, e)}
+            type="string"
+          />
+        </div>
+        <div>
+          <label htmlFor="city">Country</label>
+        </div>
+        <div>
+          <input
+            id="country"
+            onChange={(e) => inputChangeHandler(setCountry, e)}
+            type="string"
+          />
+        </div>
+        <input type="submit" />
       </form>
       {error && (
         <div>
@@ -57,7 +175,6 @@ export default function Web() {
         <div>
           <h3>Greeting</h3>
           <p>{response.message}</p>
-          <Button onClick={onReset}>Reset</Button>
         </div>
       )}
     </div>
